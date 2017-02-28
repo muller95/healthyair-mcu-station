ed75e0a894b04ae3fdff9c178837d779551a3778
muller95 <muller95@yandex.ru>
2017-02-28 23:04:00 +0300

    Added senseair k-30 support and blink for sending data

--- sketch_feb23a.ino
+++ sketch_feb23a.ino
@@ -1,8 +1,15 @@
+#include <AsyncEventSource.h>
+#include <AsyncJson.h>
+#include <AsyncWebSocket.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFSEditor.h>
+#include <StringArray.h>
+#include <WebAuthentication.h>
+#include <WebHandlerImpl.h>
+#include <WebResponseImpl.h>
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
-#include <strings.h>
-#include <stdlib.h>
 
 #define MAX_POST_DATA_SIZE 1024
 
@@ -13,39 +20,64 @@ char *wl_passwd = "Warcraft123Ab";
 String email = "muller95@yandex.ru";
 String web_passwd = "Warcraft123Ab";
 String stname = "station";
-
 int wl_status;
 
+AsyncWebServer server(80);
+
+IPAddress local_IP(192,168,4,1);
+IPAddress gateway(192,168,1,9);
+IPAddress subnet(255,255,255,0);
 
 
 WiFiClient client;
 const char* host="http://healthyair.ru/data_listener.php";
+int load_led = 12, wl_connected_led = 12, data_sent_led = 13;
 
+uint16_t k30_query()
+{
+  uint8_t len = 8;
+  uint8_t query[] = {0xFE, 0x4, 0x0, 0x3, 0x0, 0x1, 0xD5, 0xC5};
+  uint8_t resp[7];
+//  Serial.println("try get ppm");
+  while (Serial.available() == 0) {
+    Serial.write(query, len);
+//    Serial.println(k30_serial.available());
+    delay(100);
+  }
+    
+  while (Serial.available() < 7) {
+//    Serial.println(k30_serial.available());
+    delay(100);
+  }
+  
+  for (int i = 0; i < 7; i++) 
+    resp[i] = Serial.read();
 
-int load_led = 12, wl_connected_led = 13;
+  uint16_t ppm = (resp[3] << 8) | resp[4];
 
-void postData(float t, float rh)
+  return ppm;
+}
+
+void postData(float t, float rh, uint16_t co2)
 {
   //char postData[MAX_POST_DATA_SIZE];
   String postData = "email=" + email + "&passwd=" + web_passwd + "&stname=" 
-    + stname + "&t=" + String(t) + "&rh=" + String(rh) + "&co2=1000&datetime=2017-02-09 21:47:30";
+    + stname + "&t=" + String(t) + "&rh=" + String(rh) + "&co2=" + String(co2) + "&datetime=2017-02-09 21:47:30";
   
   Serial.println("Try post");
   HTTPClient http;
   http.begin("http://healthyair.ru/data_listener.php");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  //bzero(postData, MAX_POST_DATA_SIZE);
   Serial.println(t);
-//  sprintf(postData, "email=%s&passwd=%s&stname=%s&t=%lf&rh=50&co2=1000&datetime=2017-02-09 21:47:30", 
-//    email, web_passwd, stname, t);
   Serial.println(postData);
   int code = http.POST(postData);
   
   
   Serial.println(code);
   if (code >= 200 && code < 300) {
-    String resp = http.getString();
-    Serial.println(resp);
+//    String resp = http.getString();
+//    Serial.println(resp);
+    digitalWrite(data_sent_led, HIGH);
   }
   http.end();
 
@@ -71,39 +103,37 @@ void loop()
     return;
   }*/
 
+  digitalWrite(data_sent_led, LOW);
   digitalWrite(wl_connected_led, LOW);
   wl_status = WiFi.status();
   if (wl_status == WL_CONNECTED) {
     digitalWrite(wl_connected_led, HIGH);
     SHT1x sht15(4, 5);//Data, SCK
     float t = sht15.readTemperatureC();
-    float rh = sht15.readHumidity();  
-    Serial.print(" Temp = ");
-    Serial.print(t);
-    Serial.println("C");
-    Serial.print(" Humidity = ");
-    Serial.print(rh); 
-    Serial.println("%");
-    postData(t, rh);
+    float rh = sht15.readHumidity();
+    uint16_t ppm = k30_query();
+    postData(t, rh, ppm);
   }
 }
 
 void setup() 
 {
-  pinMode(load_led, OUTPUT);
-  digitalWrite(load_led, HIGH);
+  //pinMode(load_led, OUTPUT);
+  //digitalWrite(load_led, HIGH);
   pinMode(wl_connected_led, OUTPUT);
-  Serial.begin(74880);
- // Serial.setDebugOutput(true);
+  pinMode(data_sent_led, OUTPUT);
+  Serial.begin(9600);
   Serial.println("load");
+  
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready config" : "Failed config!");
+  Serial.println(WiFi.softAP("Healthy station") ? "Ready" : "Failed!");
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "HERE");
+  });
 
   WiFi.mode(WIFI_AP_STA);
   WiFi.disconnect();
   WiFi.begin("Keenetic-5802", "vNX2ambo");
-  //WiFi.mode(WIFI_STA);
-  
-  delay(100);
-
-  //while (WiFi.status() != WL_CONNECTED);
-
+  //WiFi.mode(WIFI_AP_STA);
 }
